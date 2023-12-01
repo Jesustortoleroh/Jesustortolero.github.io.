a56e8b10675f7ffcd96dd3b15b9ddfab1ff88346
Jesustortoleroh <tor19021@byui.edu>
2023-12-01 18:53:19 -0400

    Updated

--- Mel.js
+++
@@ -1,5 +0,0 @@
-const playSound = function() {
-  document.getElementById('audioelement').play();
-  document.removeEventListener('click', playSound);
-}
-document.addEventListener('click', playSound);
